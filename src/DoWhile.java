9acfe0c4e62d1f00b2438362f02299f9c4d5ba03
Moment <20390882+moment93@users.noreply.github.com>
2022-09-02 11:01:05 +0100

    update

---
+++ src/DoWhile.java
@@ -0,0 +1,11 @@
+//Demonstrate the Do-While loop
+class DoWhile{
+    public static void main(String[] args) {
+        int n = 10;
+
+        do{
+            System.out.println("Tick " + n);
+            n--;
+        }while (n > 0);
+    }
+}
