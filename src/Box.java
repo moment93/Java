9e5e35469b32eef98cb090ec0b8f759f4ef6c3fc
Moment <20390882+moment93@users.noreply.github.com>
2022-10-30 01:30:49 +0000

    update

--- src/Box.java
+++ src/Box.java
@@ -2,4 +2,30 @@ class Box{
     double width;
     double height;
     double depth;
+
+    //This is the constructor for box
+    Box(double w, double h, double d){
+        System.out.println("Constructing box");
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    Box(){
+        width = 10;
+        height = 10;
+        depth = 10;
+    }
+
+    //compute and return volume
+    double volume(){
+        return width * height * depth;
+    }
+
+    //sets dimensions of a box
+    void setDim(double w, double h, double d){
+        width = w;
+        height = h;
+        depth = d;
+    }
 }
