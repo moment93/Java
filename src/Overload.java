18e5497176dbc647a819ad0dae604a4ff5b58943
Moment <davidbucko@hotmail.co.uk>
2022-10-30 04:01:19 +0000

    update

--- src/Overload.java
+++ src/Overload.java
@@ -1,14 +1,13 @@
 class Overload{
     public static void main(String[] args) {
         OverloadDemo ob = new OverloadDemo();
-        double result;
+        int i = 88;
+
 
         //Call all versions of test
         ob.test();
-        ob.test(10);
         ob.test(10, 20);
-        result = ob.test(123.25);
-
-        System.out.println("Result of ob.test(123.25): " + result);
+        ob.test(i); //This will invoke test double
+        ob.test(123.2); //This will invoke test double
     }
 }
--- src/OverloadDemo.java
+++ src/OverloadDemo.java
@@ -5,19 +5,13 @@ class OverloadDemo{
         System.out.println("No parameters");
     }
 
-    //Overload test for one integer parameter
-    void test(int a){
-        System.out.println("a: " + a);
-    }
-
     //Overload test for two integer parameters
     void test(int a, int b){
         System.out.println("a and b: " + a + " " + b);
     }
 
     //Overload test for a double parameter
-    double test(double a){
-        System.out.println("double a: " + a);
-        return a * a;
+    void test(double a){
+        System.out.println("Inside test (double) a: " + a);
     }
 }
