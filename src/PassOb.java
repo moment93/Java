91b8224cabe13e664cedcd3179bcee85ed22da77
Moment993 <davidbucko@hotmail.co.uk>
2022-11-07 02:01:16 +0000

    update

---
+++ src/PassOb.java
@@ -0,0 +1,10 @@
+class PassOb{
+    public static void main(String[] args) {
+        Test obj1 = new Test(100,22);
+        Test obj2 = new Test(100,22);
+        Test obj3 = new Test(-1,-1);
+
+        System.out.println("obj1 == obj2: " + obj1.equalTo(obj2));
+        System.out.println("obj1 == obj3: " + obj1.equalTo(obj3));
+    }
+}
