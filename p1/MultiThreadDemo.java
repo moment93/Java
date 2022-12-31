83862c574430fef8afcc45031e6cec98a4f8b141
Moment <20390882+moment93@users.noreply.github.com>
2022-12-31 15:47:45 +0000

    update

---
+++ p1/MultiThreadDemo.java
@@ -0,0 +1,26 @@
+package p1;
+
+public class MultiThreadDemo {
+    public static void main(String[] args) {
+        NewThread nt1 = new NewThread("One");
+        NewThread nt2 = new NewThread("Two");
+        NewThread nt3 = new NewThread("Three");
+        //Start the threads.
+
+        nt1.t.start();
+        nt2.t.start();
+        nt3.t.start();
+
+        try{
+            //Waith for other threads to end
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+
+
+
+        System.out.println("Main thread exiting");
+
+    }
+}
